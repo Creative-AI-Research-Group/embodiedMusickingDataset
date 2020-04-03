efb8611621650cf276256a640f73413eb2df07fe
craigv <vear2ev2.co.uk>
2020-04-03 13:31:01 +0100

    added intial scripts for nueral network and application into music project

--- training/ML_proto_dataset.py
+++ training/ML_proto_dataset.py
@@ -14,12 +14,14 @@ def prep_dataset():
     # apply filter function
     # A) just the features we want
     df.filter(['x', 'y', 'z', 'freq', 'amp'])
-    # B) all rows with amp values greater than 0 (i.e. has a sound)
-    df = df[df['amp'] > 0]
+    # B) all rows with amp values greater than 0.2 (i.e. has a sound)
+    df = df[df['amp'] > 0.2]
+    # C) only "body" data
+    df = df[df['limb'] == '/Body']
     print (df)
     # split into input (X) and output (y) variables
-    X = df.iloc[:, 6].values # X = independent variables / input to our model (amplitude)
-    y = df.iloc[:, 2:5].values # y = dependent variable/ outputs our model predicts
+    X = df.iloc[:, 2:5].values # X = independent variables / input to our model
+    y = df.iloc[:, 6].values # y = dependent variable/ outputs our model predicts (amplitude)
     return (X, y)
 
 def train(X, y):
@@ -36,10 +38,18 @@ def train(X, y):
     # evaluate the keras model
     _, accuracy = model.evaluate(X, y)
     print('Accuracy: %.2f' % (accuracy * 100))
-    # so far loss is 60-70% and accuracy is ~ 9% = GIGO but - does it make music?
-    model.save('data/model.h5')
+    model.save('data/body_only_model.h5')
     print ('saved')
 
+"""so far loss vs accuracy are
+input = xyz, output= amp (ABOVE 0.2) (body only) loss = 66%, accuracy = 16%
+input = xyz, output= amp (body only) loss = 60%, accuracy = 11%
+input = xyz, output= amp (all limbs) loss = 70%, accuracy = 9%
+input = amp; output = xyz loss = -21169.1751% and accuracy is 0%  
+
+Overall - GIGO but - does it make music?
+
+"""
 
 # --------- programme starts here -----------
 # if '__name__' == '__main__':
--- training/NN_music_performance.py
+++ training/NN_music_performance.py
@@ -1,6 +1,7 @@
 import pandas as pd
-from keras.models import Sequential, load_model
+from keras.models import load_model
 from random import randrange
+import numpy as np
 
 def build_model():
 	# build model from trained files
@@ -8,13 +9,30 @@ def build_model():
 	# make probability predictions with the model
 	return model
 
-# def prediction(model, X):
-# 	pdct = model.predict(X)
-# 	return (pdct)
+def prediction(model, X):
+	reshape
+	pdct = model.predict(X)
+	return pdct
 
+def test_array():
+	rang = 200
+	for i in range(rang):
+		sampl = np.random.uniform(low=0, high=1, size=(200))
+	print (sampl)
+	return sampl
 
 #code starts here
 model = build_model()
-for i in range(5):
-	predict = model.predict(X)
-	print(predict)
+array = test_array()
+for l in range (len(array)):
+	num = array[l]
+	pdct = prediction(model, num)
+	print ('num= ', num, 'predict= ', pdct)
+
+
+# for i in range(5):
+# 	X = randrange (200)
+# 	X /= 100
+# 	X = str(X)
+# 	predict = model.predict(X)
+# 	print(predict)
