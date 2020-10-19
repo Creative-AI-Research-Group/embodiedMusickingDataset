085d68854c4fe988040bc379a829b1963b9c758b
fabrizio poltronieri <fabrizio@fabriziopoltronieri.com>
2020-10-19 23:54:17 +0200

    basic UI and camera working

--- blue haze/main.py
+++ blue haze/main.py
@@ -6,23 +6,103 @@
 # Craig Vear - cvear@dmu.ac.uk
 #
 
-from PySide2.QtWidgets import (QApplication, QLabel, QPushButton,
-                               QVBoxLayout, QWidget, QLineEdit,
-                               QComboBox)
+from PySide2.QtWidgets import *
+from PySide2.QtMultimedia import *
+from PySide2.QtMultimediaWidgets import QCameraViewfinder
 from PySide2.QtCore import Slot, Qt
 
-import cv2
 import sys
 
 
 class MainWindow(QWidget):
     def __init__(self):
         QWidget.__init__(self)
+
+        self.session_name = QLineEdit()
+        self.video_file_path = QLineEdit()
+        self.list_cameras = QComboBox()
+        self.record_stop_button = QPushButton('Record session')
+
+        self.view_finder = QCameraViewfinder()
+
+        self.get_list_cameras()
         self.setup_ui()
 
+        self.camera = QCamera(self.list_cameras.currentData())
+        self.camera.setViewfinder(self.view_finder)
+        self.camera.start()
+
     def setup_ui(self):
-        self.session_name = QLineEdit()
-        self.camera = QComboBox()
+        # fields
+        fields_group_box = QGroupBox()
+        fields = QGridLayout()
+        fields.setSpacing(8)
+
+        # session name
+        session_name_label = QLabel('Session name: ')
+
+        # video file path
+        video_path_file_label = QLabel('Video path: ')
+        folder_browser_button = QPushButton('Browse directories')
+        # connect the button signal
+        folder_browser_button.clicked.connect(self.show_folder_browser)
+
+        # camera selection
+        list_cameras_label = QLabel('Available cameras: ')
+
+        # fields layout
+        fields.addWidget(session_name_label, 0, 0)
+        fields.addWidget(self.session_name, 0, 1)
+        fields.addWidget(list_cameras_label, 2, 0)
+        fields.addWidget(self.list_cameras, 2, 1)
+        fields.addWidget(video_path_file_label, 1, 0)
+        fields.addWidget(self.video_file_path, 1, 1)
+        fields.addWidget(folder_browser_button, 1, 2)
+
+        fields_group_box.setLayout(fields)
+
+        # viewfinder
+        view_finder_group_box = QGroupBox()
+        view_finder_layout = QGridLayout()
+        view_finder_layout.addWidget(self.view_finder, 1, 1)
+        view_finder_group_box.setLayout(view_finder_layout)
+
+        # button
+        record_button_group_box = QGroupBox()
+        record_button_layout = QGridLayout()
+        empty_label = QLabel(' ')
+        record_button_layout.addWidget(empty_label, 0, 1)
+        record_button_layout.addWidget(empty_label, 0, 2)
+        record_button_layout.addWidget(empty_label, 0, 3)
+        record_button_layout.addWidget(self.record_stop_button, 0, 4)
+        record_button_group_box.setLayout(record_button_layout)
+
+        # layout
+        layout = QVBoxLayout()
+        layout.addWidget(fields_group_box)
+        layout.addWidget(view_finder_group_box)
+        layout.addWidget(record_button_group_box)
+        self.setLayout(layout)
+
+        # connect the button signal
+        self.record_stop_button.clicked.connect(self.action_record_stop_button)
+
+    @Slot()
+    def action_record_stop_button(self):
+        print('Button pressed')
+
+    @Slot()
+    def show_folder_browser(self):
+        folder_dialog = QFileDialog()
+        folder_dialog.setOption(QFileDialog.ShowDirsOnly)
+        folder_dialog.setFileMode(QFileDialog.Directory)
+        if folder_dialog.exec_():
+            self.video_file_path.setText(folder_dialog.directory().absolutePath())
+
+    def get_list_cameras(self):
+        # list the available cameras
+        for camera_info in QCameraInfo.availableCameras():
+            self.list_cameras.addItem(camera_info.description(), camera_info)
 
 
 if __name__ == '__main__':
@@ -30,7 +110,7 @@ if __name__ == '__main__':
 
     widget = MainWindow()
     widget.setWindowTitle('Blue Haze')
-    widget.resize(800, 600)
+    widget.setFixedSize(1350, 950)
     widget.show()
 
     sys.exit(app.exec_())
