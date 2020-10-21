f4ecc667a3354abeb184f095247814bb39e9ecca
craigvear <rdjaj&5Bxc>
2020-10-21 09:13:36 -0700

    adding data streaming to main.py todo - work on Qthreads

--- blue haze/main.py
+++ blue haze/main.py
@@ -9,18 +9,22 @@
 from PySide2.QtWidgets import *
 from PySide2.QtMultimedia import *
 from PySide2.QtMultimediaWidgets import QCameraViewfinder
-from PySide2.QtCore import Slot, Qt
+from PySide2.QtCore import Slot, Qt, QThread
 from glob import glob
 import os
 import sys
 from bitalino import BITalino
 import argparse
-import time
 import numpy as np
-
 import brainflow
 from brainflow.board_shim import BoardShim, BrainFlowInputParams
 from brainflow.data_filter import DataFilter, FilterTypes, AggOperations
+import util as cm
+import time
+import pyrealsense2 as rs
+import math
+from skeletontracker import skeletontracker
+
 
 # Bitalino setup parameters
 # The macAddress variable on Windows can be "XX:XX:XX:XX:XX:XX" or "COMX"
@@ -281,6 +285,130 @@ def brainbit_terminate():
     board.stop_stream()
     board.release_session()
 
+def render_ids_3d(
+    render_image, skeletons_2d, depth_map, depth_intrinsic, joint_confidence
+):
+    thickness = 1
+    text_color = (255, 255, 255)
+    rows, cols, channel = render_image.shape[:3]
+    distance_kernel_size = 5
+    # calculate 3D keypoints and display them
+    for skeleton_index in range(len(skeletons_2d)):
+        skeleton_2D = skeletons_2d[skeleton_index]
+        joints_2D = skeleton_2D.joints
+        did_once = False
+        for joint_index in range(len(joints_2D)):
+            if did_once == False:
+                cv2.putText(
+                    render_image,
+                    "id: " + str(skeleton_2D.id),
+                    (int(joints_2D[joint_index].x), int(joints_2D[joint_index].y - 30)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    0.55,
+                    text_color,
+                    thickness,
+                )
+                did_once = True
+            # check if the joint was detected and has valid coordinate
+            if skeleton_2D.confidences[joint_index] > joint_confidence:
+                distance_in_kernel = []
+                low_bound_x = max(
+                    0,
+                    int(
+                        joints_2D[joint_index].x - math.floor(distance_kernel_size / 2)
+                    ),
+                )
+                upper_bound_x = min(
+                    cols - 1,
+                    int(joints_2D[joint_index].x + math.ceil(distance_kernel_size / 2)),
+                )
+                low_bound_y = max(
+                    0,
+                    int(
+                        joints_2D[joint_index].y - math.floor(distance_kernel_size / 2)
+                    ),
+                )
+                upper_bound_y = min(
+                    rows - 1,
+                    int(joints_2D[joint_index].y + math.ceil(distance_kernel_size / 2)),
+                )
+                for x in range(low_bound_x, upper_bound_x):
+                    for y in range(low_bound_y, upper_bound_y):
+                        distance_in_kernel.append(depth_map.get_distance(x, y))
+                median_distance = np.percentile(np.array(distance_in_kernel), 50)
+                depth_pixel = [
+                    int(joints_2D[joint_index].x),
+                    int(joints_2D[joint_index].y),
+                ]
+                if median_distance >= 0.3:
+                    point_3d = rs.rs2_deproject_pixel_to_point(
+                        depth_intrinsic, depth_pixel, median_distance
+                    )
+                    point_3d = np.round([float(i) for i in point_3d], 3)
+                    point_str = [str(x) for x in point_3d]
+                    cv2.putText(
+                        render_image,
+                        str(point_3d),
+                        (int(joints_2D[joint_index].x), int(joints_2D[joint_index].y)),
+                        cv2.FONT_HERSHEY_DUPLEX,
+                        0.4,
+                        text_color,
+                        thickness,
+                    )
+
+def skeleton_setup():
+    config = rs.config()
+    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
+    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+
+    # Start the realsense pipeline
+    pipeline = rs.pipeline()
+    pipeline.start()
+
+    # Create align object to align depth frames to color frames
+    align = rs.align(rs.stream.color)
+
+    # Get the intrinsics information for calculation of 3D point
+    unaligned_frames = pipeline.wait_for_frames()
+    frames = align.process(unaligned_frames)
+    depth = frames.get_depth_frame()
+    depth_intrinsic = depth.profile.as_video_stream_profile().intrinsics
+
+    # Initialize the cubemos api with a valid license key in default_license_dir()
+    skeletrack = skeletontracker(cloud_tracking_api_key="")
+    joint_confidence = 0.2
+
+    return pipeline, skeletrack, joint_confidence
+
+def skeleton_read(pipeline, skeletrack, joint_confidence):
+    while True:
+        # Create a pipeline object. This object configures the streaming camera and owns it's handle
+        unaligned_frames = pipeline.wait_for_frames()
+        frames = align.process(unaligned_frames)
+        depth = frames.get_depth_frame()
+        color = frames.get_color_frame()
+        if not depth or not color:
+            continue
+
+        # Convert images to numpy arrays
+        depth_image = np.asanyarray(depth.get_data())
+        color_image = np.asanyarray(color.get_data())
+
+        # perform inference and update the tracking id
+        skeletons = skeletrack.track_skeletons(color_image)
+
+        # render the skeletons on top of the acquired image and display it
+        color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
+        cm.render_result(skeletons, color_image, joint_confidence)
+        render_ids_3d(
+            color_image, skeletons, depth, depth_intrinsic, joint_confidence
+        )
+        cv2.imshow(window_name, color_image)
+        if cv2.waitKey(1) == 27:
+            break
+
+def skeleton_terminate():
+    pipeline.stop()
 
 if __name__ == '__main__':
     # BITalino startup
@@ -288,6 +416,9 @@ if __name__ == '__main__':
 
     #BraibBit startup
     board, args = brainbit_setup()
+
+    # RealSense & Skeleton startup
+    pipeline, skeletrack, joint_confidence = skeleton_setup()
 
     # UI startup
     app = QApplication(sys.argv)
@@ -298,11 +429,11 @@ if __name__ == '__main__':
     widget.show()
 
 
-
     #Terminations
 
     brainbit_terminate()
     bitalino_terminate()
+    skeleton_terminate()
 
     # Close UI
     sys.exit(app.exec_())
