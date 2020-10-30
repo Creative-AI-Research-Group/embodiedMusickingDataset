be28e8914ffcc393e2e78de9a071c61d2f921730
fabrizio poltronieri <fabrizio@fabriziopoltronieri.com>
2020-10-30 17:27:42 +0100

    check platform

---
+++ blue haze/checkPlatform.py
@@ -0,0 +1,16 @@
+#
+# Blue Haze
+# 30 Oct 2020
+#
+# Fabrizio Poltronieri - fabrizio.poltronieri@dmu.ac.uk
+# Craig Vear - cvear@dmu.ac.uk
+#
+
+import platform
+
+
+def check_platform():
+    # Linux: Linux
+    # Mac: Darwin
+    # Windows: Windows
+    return platform.system()
