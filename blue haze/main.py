589d9657972562a26d85461a0e57d1581c4b1ec7
Fabrizio Poltronieri <fabrizio@fabriziopoltronieri.com>
2020-11-18 15:47:29 +0000

    dark mode

--- blue haze/main.py
+++ blue haze/main.py
@@ -15,6 +15,7 @@
 from PySide2.QtWidgets import *
 from PySide2.QtMultimedia import *
 from PySide2.QtMultimediaWidgets import QCameraViewfinder
+from PySide2.QtGui import QPalette, QColor
 from PySide2.QtCore import Slot, Qt, QDir
 from glob import glob
 from playBackTrack import PlayBackTrack
@@ -27,6 +28,24 @@ import nest_asyncio
 
 import modules.utils as utls
 import modules.config as cfg
+
+
+def dark_palette():
+    dark_palette_colours = QPalette()
+    dark_palette_colours.setColor(QPalette.Window, QColor(53, 53, 53))
+    dark_palette_colours.setColor(QPalette.WindowText, Qt.white)
+    dark_palette_colours.setColor(QPalette.Base, QColor(25, 25, 25))
+    dark_palette_colours.setColor(QPalette.AlternateBase, QColor(53, 53, 53))
+    dark_palette_colours.setColor(QPalette.ToolTipBase, Qt.white)
+    dark_palette_colours.setColor(QPalette.ToolTipText, Qt.white)
+    dark_palette_colours.setColor(QPalette.Text, Qt.white)
+    dark_palette_colours.setColor(QPalette.Button, QColor(53, 53, 53))
+    dark_palette_colours.setColor(QPalette.ButtonText, Qt.white)
+    dark_palette_colours.setColor(QPalette.BrightText, Qt.red)
+    dark_palette_colours.setColor(QPalette.Link, QColor(42, 130, 218))
+    dark_palette_colours.setColor(QPalette.Highlight, QColor(42, 130, 218))
+    dark_palette_colours.setColor(QPalette.HighlightedText, Qt.black)
+    return dark_palette_colours
 
 
 class MainWindow(QMainWindow):
@@ -369,6 +388,8 @@ class MainWindow(QMainWindow):
 if __name__ == '__main__':
     # UI startup
     app = QApplication()
+    app.setStyle('Fusion')
+    app.setPalette(dark_palette())
 
     window = MainWindow()
     window.show()
