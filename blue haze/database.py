4b74f534f811b2d6f372438a7f9608225f84a4b1
Fabrizio Poltronieri <fabrizio@fabriziopoltronieri.com>
2020-12-15 13:47:24 +0000

    MongoDB object restructured

--- blue haze/database.py
+++ blue haze/database.py
@@ -53,21 +53,21 @@ class Database:
                               bitalino_data,
                               brainbit_data,
                               skeleton_data):
-        post = {'session_id': self.session_id,
-                'session_name': self.session_name,
-                'timestamp': timestamp,
-                'delta_time': delta_time,
-                'audio_file': self.audio_file,
-                'mic_volume': self.mic_volume,
-                'video_file': self.video_file,
-                'backing_track_file': self.backing_track_file,
-                'backing_track_position': backing_track_position,
-                'bitalino': bitalino_data,
-                'brainbit': brainbit_data,
-                'skeleton_data': skeleton_data,
-                'flow_level': None,
-                'date': datetime.datetime.utcnow(),
-                'last_update': None}
+        post = {'session': {'id': self.session_id,
+                            'name': self.session_name,
+                            'date': datetime.datetime.utcnow(),
+                            'last_update': None},
+                'sync': {'timestamp': timestamp,
+                         'delta': delta_time,
+                         'backing_track_position': backing_track_position},
+                'files': {'audio': {'file': self.audio_file,
+                                    'mic_volume': self.mic_volume},
+                          'video': self.video_file,
+                          'backing_track': self.backing_track_file},
+                'hardware': {'bitalino': bitalino_data,
+                             'brainbit': brainbit_data,
+                             'skeleton': skeleton_data},
+                'flow': None}
         result = await self.collection.insert_one(post)
 
     def close(self):
